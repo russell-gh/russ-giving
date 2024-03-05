5a8bf4647ce30d2abd5eb6bc53507ea4ee01cee6
Your Name <you@example.com>
2024-03-05 21:35:35 +0000

    gsap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
+import { useLayoutEffect, useEffect, useRef } from "react";
 import Nav from "./Nav";
 
 const Header = () => {
+  // useLayoutEffect(() => { //occurs post dom update but before paint
+  //   gsap.to(h1Ref.current, { backgroundColor: "red", duration: 5 });
+  // }, []);
+
+  const h1Ref = useRef();
+
   return (
     <div>
-      <h1>Russ Giving</h1>
+      <h1 ref={h1Ref}>Russ Giving</h1>
       <p>Please give money!</p>
-      <Nav />
+      <Nav h1Ref={h1Ref} />
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
+import gsap from "gsap";
 
-const Nav = () => {
+const Nav = ({ h1Ref }) => {
+  const onClick = () => {
+    console.log("onClick", h1Ref);
+    gsap.to(h1Ref.current, {
+      backgroundColor: "red",
+      duration: 5,
+      onComplete: () => {},
+    });
+  };
+
   return (
     <>
+      <button onClick={onClick}>Set H1 red from child!</button>
       <Link to="/">Home</Link> <Link to="/account">Account</Link>
     </>
   );
